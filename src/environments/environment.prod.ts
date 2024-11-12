8c13e7f0c1389a9f8a01429ff3c3ba104459857b
Vulcan875 <borheneddine.benattia@esprit.tn>
2024-11-12 02:12:25 +0100

    angular jzxi brhi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    apiUrl: ' http://192.168.33.10:8085/Foyer' // or your production API URL
+    apiUrl: 'http://192.168.50.4:8085/Foyer' // or your production API URL
   };
   
